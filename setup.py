6eccca3b5790950d0346be0777336a91a8183020
Madison May <madison@indico.io>
2018-06-26 14:14:09 -0400

    FIX: requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,21 @@ IndicoAPI setup
 from sys import version_info
 from setuptools import setup, find_packages
 
+REQUIREMENTS = [
+    "tqdm>=4.0.0",
+    "pandas>=0.20.0",
+    "numpy>=1.13.0",
+    "scikit-learn>=0.18.0",
+    "joblib>=0.11",
+    "ftfy>=4.4.0",
+    "spacy>=2.0.0",
+    "enso>=0.1.1",
+    "tensorflow-gpu>=1.6.0",
+]
 
 setup(
     name="finetune",
     packages=find_packages(),
-    setup_requires=open('requirements.txt').readlines(),
     version="0.1.0",
+    install_requires=REQUIREMENTS,
 )
