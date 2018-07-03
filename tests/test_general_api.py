0f7826d473d8df6f00892cd819e47704b519daba
work <work@Bens-MacBook.local>
2018-07-03 12:02:28 +0100

    FIX: PR comments and fix test names

--- tests/test_general_api.py
+++ tests/test_general_api.py
@@ -18,12 +18,12 @@ SST_FILENAME = "SST-binary.csv"
 
 
 class TestLanguageModelClassifier(unittest.TestCase):
-
     n_sample = 100
     n_hidden = 768
     dataset_path = os.path.join(
         enso.config.DATA_DIRECTORY, 'Classify', 'SST-binary.csv'
     )
+
     @classmethod
     def _download_sst(cls):
         """
@@ -41,52 +41,50 @@ class TestLanguageModelClassifier(unittest.TestCase):
             filename=SST_FILENAME
         )
 
-
     @classmethod
     def setUpClass(cls):
         cls._download_sst()
 
     def setUp(self):
+        self.save_file_autosave = 'tests/saved-models/autosave_path'
+        self.save_file = 'tests/saved-models/test-save-load'
+        self.model = LanguageModelGeneralAPI(verbose=False, autosave_path=save_file_autosave)
+
         self.dataset = pd.read_csv(self.dataset_path)
+        train_sample = self.dataset.sample(n=self.n_sample)
+        valid_sample = self.dataset.sample(n=self.n_sample)
+        self.text_data_train = list(zip(train_sample.Text, train_sample.Text, train_sample.Text))
+        self.text_data_valid = list(zip(valid_sample.Text, valid_sample.Text, valid_sample.Text))
+        self.train_targets = train_sample.Target
         tf.reset_default_graph()
 
     def test_multifield_classify(self):
         """
-        Ensure fit predict works.
+        Ensure fit predict works on classification with multi inputs
         Ensure saving + loading does not cause errors
         Ensure saving + loading does not change predictions
         """
-        save_file_autosave = 'tests/saved-models/autosave_path'
-        save_file = 'tests/saved-models/test-save-load'
-        model = LanguageModelGeneralAPI(verbose=False, autosave_path=save_file_autosave)
-        train_sample = self.dataset.sample(n=self.n_sample)
-        valid_sample = self.dataset.sample(n=self.n_sample)
-        model.fit([train_sample.Text] * 3, train_sample.Target)
-        self.assertTrue(model.is_classification)
-        predictions = model.predict([valid_sample.Text] * 3)
-        model.save(save_file)
-        model = LanguageModelGeneralAPI.load(save_file)
-        new_predictions = model.predict([valid_sample.Text] * 3)
-        for i, prediction in enumerate(predictions):
-            self.assertEqual(prediction, new_predictions[i])
+        self.model.fit(self.text_data_train, self.train_targets)
+        self.assertTrue(self.model.is_classification)
+        predictions = self.model.predict(self.text_data_valid)
+        self.model.save(self.save_file)
+        model = LanguageModelGeneralAPI.load(self.save_file)
+        new_predictions = model.predict(self.text_data_valid)
+        for new_pred, old_pred in zip(new_predictions, predictions):
+            self.assertEqual(new_pred, old_pred)
 
-
-    def test_multifield_classify(self):
+    def test_multifield_regression(self):
         """                                                                                                                                                                         
-        Ensure fit predict works.                                                                                                                                                   
+        Ensure fit predict works with regression targets and multiple inputs.
         Ensure saving + loading does not cause errors                                                                                                                               
         Ensure saving + loading does not change predictions                                                                                                                         
         """
-        save_file_autosave = 'tests/saved-models/autosave_path'
-        save_file = 'tests/saved-models/test-save-load'
-        model = LanguageModelGeneralAPI(verbose=False, autosave_path=save_file_autosave)
-        train_sample = self.dataset.sample(n=self.n_sample)
-        valid_sample = self.dataset.sample(n=self.n_sample)
-        model.fit([train_sample.Text] * 3, [np.random.random() for _ in train_sample.Target])
-        self.assertTrue(not model.is_classification)
-        predictions = model.predict([valid_sample.Text] * 3)
-        model.save(save_file)
-        model = LanguageModelGeneralAPI.load(save_file)
-        new_predictions = model.predict([valid_sample.Text] * 3)
-        for i, prediction in enumerate(predictions):
-            self.assertEqual(prediction, new_predictions[i])
+
+        self.model.fit(self.text_data_train, [np.random.random() for _ in self.train_targets])
+        self.assertTrue(not self.model.is_classification)
+        predictions = self.model.predict(self.text_data_valid)
+        self.model.save(self.save_file)
+        model = LanguageModelGeneralAPI.load(self.save_file)
+        new_predictions = model.predict(self.text_data_valid)
+        for new_pred, old_pred in zip(new_predictions, predictions):
+            self.assertEqual(new_pred, old_pred)
