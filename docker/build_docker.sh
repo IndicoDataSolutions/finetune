d8ee1fa760acddeccb8fb558ec17bc29a8486a29
Madison May <madison@indico.io>
2018-07-11 08:42:53 -0400

    FIX: remove requirement that docker scripts are run from project root

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,1 +1,5 @@
-docker build -t finetune --file ./docker/Dockerfile . 
+#!/bin/bash
+
+DOCKER_DIR=`dirname $0`
+PROJECT_ROOT=$( dirname $DOCKER_DIR )
+docker build -t finetune --file $DOCKER_DIR/Dockerfile $PROJECT_ROOT 
--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+DOCKER_DIR=`dirname $0`
+PROJECT_ROOT=$( dirname $DOCKER_DIR )
 docker run --runtime=nvidia -d -v $(pwd):/Finetune finetune
