1be269ba6a94fa1590d935cf0d6f59d6a714c87f
Madison May <madison@indico.io>
2018-07-12 11:07:55 -0400

    HELP

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
 
 DOCKER_DIR=`dirname $0`
 PROJECT_ROOT=$(get_abs_filename "$( dirname $DOCKER_DIR )")
