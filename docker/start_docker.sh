24a4127e1c45af3b55a875bdf93b78cb20e2be96
Madison May <madison@indico.io>
2018-07-12 09:43:45 -0400

    TAG container as `finetune`

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -6,4 +6,4 @@ get_abs_filename() {
 
 DOCKER_DIR=`dirname $0`
 PROJECT_ROOT=$(get_abs_filename "$( dirname $DOCKER_DIR )")
-docker run --runtime=nvidia -d -v $PROJECT_ROOT:/Finetune finetune
+docker run --runtime=nvidia -d -v $PROJECT_ROOT:/Finetune --name finetune finetune 
