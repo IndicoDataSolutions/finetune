FIX: stop_gradient location

finetune/base.py
@@ -525,7 +525,7 @@ class BaseModel(object, metaclass=ABCMeta):
         if self.target_type == SEQUENCE_LABELING:
             self.Y = tf.placeholder(tf.int32, [None, self.config.max_length])  # classification targets
         else:
-            self.Y = tf.stop_gradient(tf.placeholder(tf.float32, [None, self.target_dim or 1]))  # classification targets
+            self.Y = tf.placeholder(tf.float32, [None, self.target_dim or 1])  # classification targets
 
     def generate_text(self, max_length=None, seed_text=''):
         """

finetune/network_modules.py
@@ -115,7 +115,10 @@ def classifier(hidden, targets, n_classes, dropout_placeholder, config, train=Fa
     with tf.variable_scope('model', reuse=reuse):
         hidden = dropout(hidden, config.clf_p_drop, train, dropout_placeholder)
         clf_logits = perceptron(hidden, n_classes, config)
-        clf_losses = tf.nn.softmax_cross_entropy_with_logits_v2(logits=clf_logits, labels=targets)
+        clf_losses = tf.nn.softmax_cross_entropy_with_logits_v2(
+            logits=clf_logits,
+            labels=tf.stop_gradient(targets)
+        )
         return {
             'logits': clf_logits,
             'losses': clf_losses