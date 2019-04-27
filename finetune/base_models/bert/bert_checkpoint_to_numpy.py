b9c97ca98d5e210627d111ab3f5f247e0e337dd4
benleetownsend <benlt@hotmail.co.uk>
2019-04-27 22:20:33 +0100

    ADD: fix for checkpoint conversion script

--- finetune/base_models/bert/bert_checkpoint_to_numpy.py
+++ finetune/base_models/bert/bert_checkpoint_to_numpy.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     OUTPUT_NAME = "/Users/work/git/finetune-transformer-lm/finetune/model/bert/bert_small_multi_cased.jl"
     reader = tf.train.NewCheckpointReader(PATTERN)
     keys = list(reader.get_variable_to_shape_map().keys())
-    output_dict = {'model/featurizer/' + k: reader.get_tensor(k) for k in keys}
+    output_dict = {'model/featurizer/' + k + ":0": reader.get_tensor(k) for k in keys}
     jl.dump(output_dict, OUTPUT_NAME)
 
