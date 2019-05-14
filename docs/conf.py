69d63edd0fb0e6543e356141e2e60f1520a13df5
Madison May <madison@indico.io>
2019-05-14 16:52:16 -0400

    UPDATE: 0.6.4

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Madison May, Ben Townsend'
 version, __version__ = ('0.6',) * 2
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.3'
+release = '0.6.4'
 
 # -- General configuration ---------------------------------------------------
 
