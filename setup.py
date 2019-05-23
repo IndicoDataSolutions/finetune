bf64fc2ddd12364a11d03f270358fe82d0df9e6a
Madison May <madison@indico.io>
2019-05-23 18:05:42 -0400

    UPDATE: 0.6.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ REQUIREMENTS = [
 setup(
     name="finetune",
     packages=find_packages(),
-    version="0.6.5",
+    version="0.6.6",
     install_requires=REQUIREMENTS,
     extras_require={
         "tf": ["tensorflow>=1.13.0"],
