46a27b9f32420975c3c1dcf964eb981e1aefd407
benleetownsend <benlt@hotmail.co.uk>
2019-06-03 11:56:47 +0100

    ADD: wwm bert

--- finetune/base_models/bert/model.py
+++ finetune/base_models/bert/model.py
@@ -60,6 +60,33 @@ class BERTModelLargeCased(SourceModel):
     ]
 
 
+class BERTModelLargeWWMCased(SourceModel):
+    is_bidirectional = True
+    encoder = BERTEncoderLarge
+    featurizer = bert_featurizer
+    settings = {
+        'n_embed': 1024,
+        'n_epochs': 8,
+        'n_heads': 16,
+        'n_layer': 24,
+        'num_layers_trained': 24,
+        'act_fn': "gelu",
+        "lr_warmup": 0.1,
+        "lr": 1e-5,
+        "l2_reg": 0.01,
+        'bert_intermediate_size': 4096,
+        "base_model_path": os.path.join("bert", "bert_wwm_large_cased.jl"),
+    }
+    required_files = [
+        {
+            'file': os.path.join(FINETUNE_BASE_FOLDER, 'model', 'bert', filename),
+            'url': urljoin(BERT_BASE_URL, filename)
+        } for filename in [
+            "bert_wwm_large_cased.jl", "vocab_large.txt"
+        ]
+    ]
+
+
 class BERTModelMultilingualCased(SourceModel):
     is_bidirectional = True
     encoder = BERTEncoderMultuilingal
