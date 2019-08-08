77fefda41c34cdd16ab3024e428f67f6dbb805d4
matthewbayer <matthew.bayer@indico.io>
2019-08-08 12:05:10 -0400

    FIX: my fix to incorrect tqdm total

--- finetune/base.py
+++ finetune/base.py
@@ -499,7 +499,7 @@ class BaseModel(object, metaclass=ABCMeta):
                 estimator.predict(
                     input_fn=input_fn, predict_keys=predict_keys, hooks=hooks
                 ),
-                total=length or n_examples,
+                total=n_examples or length,
                 desc="Inference",
             )
             try:
