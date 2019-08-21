809f2d70b83157c201d2517238e03ef6e42796a9
Madison May <madison@indico.io>
2019-08-21 08:54:34 -0400

    ADD: GPT2Large base model

--- finetune/base_models/__init__.py
+++ finetune/base_models/__init__.py
@@ -29,7 +29,7 @@ class SourceModel(metaclass=ABCMeta):
 
 
 from finetune.base_models.gpt.model import GPTModel, GPTModelSmall
-from finetune.base_models.gpt2.model import GPT2Model, GPT2Model345
+from finetune.base_models.gpt2.model import GPT2Model, GPT2Model345, GPT2Model762
 from finetune.base_models.textcnn.model import TextCNNModel
 from finetune.base_models.bert.model import BERTModelCased, BERTModelLargeCased, RoBERTa
 
@@ -37,6 +37,7 @@ from finetune.base_models.bert.model import BERTModelCased, BERTModelLargeCased,
 GPT = GPTModel
 GPT2 = GPT2Small = GPT2Model
 GPT2Medium = GPT2Model345
+GPT2Large = GPT2Model762
 TextCNN = TextCNNModel
 BERT = BERTModelCased
 BERTLarge = BERTModelLargeCased
