307bc37a295d63948ade44f099cb5299921eb40b
benleetownsend <benlt@hotmail.co.uk>
2019-09-04 16:30:44 +0100

    FIX: single gpu support

--- finetune/datasets/reuters.py
+++ finetune/datasets/reuters.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         test_size=0.7,
         random_state=42
     )
-    model = SequenceLabeler(base_model=RoBERTa, batch_size=2, val_size=0., max_length=16, chunk_long_sequences=True, subtoken_predictions=True)
+    model = SequenceLabeler(base_model=RoBERTa, batch_size=1, val_size=0., max_length=16, chunk_long_sequences=True, subtoken_predictions=True)
     model.fit(trainX, trainY)
     predictions = model.predict(testX)
     print(predictions)
