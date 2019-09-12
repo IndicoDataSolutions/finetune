98313c30478bede9f551bd19467c71062359e1bd
benleetownsend <benlt@hotmail.co.uk>
2019-09-12 14:14:33 +0100

    FIX: bug fixes in saver and publicly expose the choice between tf and fused kernel

--- finetune/base_models/oscar/ra.py
+++ finetune/base_models/oscar/ra.py
@@ -45,21 +45,31 @@ def batch_to_time(value, dilation):
                           [tf.div(shape[0], dilation), -1, shape[2]])
 
 
-def _dilated_causal_max_pool(value, kernel_size, dilation, padding="left"):
-    pool_op = lambda x, padding: _causalmax(x, dim=1, pool_len=kernel_size, padding_type=padding)
+def _dilated_causal_max_pool(value, kernel_size, dilation):
+    pool_op = lambda x: _causalmax(x, dim=1, pool_len=kernel_size)
     with tf.name_scope("causal_pool"):
         if dilation > 1:
             transformed, pad_elements = _time_to_batch(value, dilation, pad_with=-1e4)
-            pooled = pool_op(transformed, padding)
+            pooled = pool_op(transformed)
             restored = batch_to_time(pooled, dilation)
             restored = restored[:, :tf.shape(restored)[1] - pad_elements, :]
         else:
-            restored = pool_op(value, padding)
+            restored = pool_op(value)
 
         restored.set_shape(value.get_shape())
         return restored
 
+def recursive_agg_tf(value, kernel_size, pool_len):
+    full_pool_len = pool_len
+    num_pooling_ops = int(math.ceil(math.log(full_pool_len, kernel_size)))
+    
+    intermediate_vals = []
+    for i in range(num_pooling_ops):
+        value = _dilated_causal_max_pool(value, kernel_size=kernel_size, dilation=kernel_size ** i)
+        intermediate_vals.append(value)
 
+    return tf.stack(intermediate_vals, 2)
+    
 try:
     # TODO: figure out how to ship this in a way that will build with finetune.
     ra_module = tf.load_op_library(os.path.join(os.path.dirname(__file__), 'lib_ra.so'))
@@ -74,13 +84,4 @@ try:
 except NotFoundError:
     warnings.warn("Failed to load Oscar's optimised kernels. Falling back to tensorflow version.")
 
-    def recursive_agg(value, kernel_size, pool_len):
-        full_pool_len = pool_len
-        num_pooling_ops = int(math.ceil(math.log(full_pool_len, kernel_size)))
-
-        intermediate_vals = []
-        for i in range(num_pooling_ops):
-            value = _dilated_causal_max_pool(value, kernel_size=kernel_size, dilation=kernel_size ** i)
-            intermediate_vals.append(value)
-
-        return tf.stack(intermediate_vals, 2)
+    recursive_agg = recursive_agg_tf
