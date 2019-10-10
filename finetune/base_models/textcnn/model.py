82d102bf6b202f97297ed289a517e8f09b30625b
benleetownsend <benlt@hotmail.co.uk>
2019-10-10 12:31:23 +0100

    FIX: early stopping for textcnn model

--- finetune/base_models/textcnn/model.py
+++ finetune/base_models/textcnn/model.py
@@ -19,6 +19,7 @@ class TextCNNModel(SourceModel):
         "n_epochs": 100,
         "keep_best_model": True,
         "early_stopping_steps": 100,
+        "val_size": "auto"
         "num_layers_trained": 1,
         "kernel_sizes": kernel_sizes,
         "num_filters_per_size": 2,
