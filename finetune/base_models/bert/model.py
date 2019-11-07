6150a4536f168b76e7d29915569f3a73b751e637
Madison May <madison@indico.io>
2019-11-07 08:33:14 -0500

    ADD: DistilRoBERTa

--- finetune/base_models/bert/model.py
+++ finetune/base_models/bert/model.py
@@ -227,3 +227,43 @@ class DistilBERT(SourceModel):
         }
         for filename in ["distillbert.jl", "distillbert_vocab.txt"]
     ]
+
+
+class DistilRoBERTa(SourceModel):
+    is_bidirectional = True
+    encoder = RoBERTaEncoder
+    featurizer = bert_featurizer
+    settings = {
+        "max_length": 512,
+        "n_embed": 768,
+        "n_epochs": 8,
+        "n_heads": 12,
+        "n_layer": 6,
+        "num_layers_trained": 6,
+        "act_fn": "gelu",
+        "lr_warmup": 0.1,
+        "lr": 1e-5,
+        "l2_reg": 0.01,
+        "bert_use_pooler": False,
+        "bert_use_type_embed": False,
+        "bert_intermediate_size": 3072,
+        "base_model_path": os.path.join("bert", "distilroberta.jl"),
+    }
+    required_files = [
+        {
+            "file": os.path.join(FINETUNE_BASE_FOLDER, "model", "bert", 'distilroberta.jl'),
+            "url": urljoin(BERT_BASE_URL, 'distilroberta.jl'),
+        },
+        {
+            "file": os.path.join(FINETUNE_BASE_FOLDER, "model", "bert", "dict.txt"),
+            "url": urljoin(ROBERTA_BASE_URL, "dict.txt"),
+        },
+        {
+            "file": os.path.join(FINETUNE_BASE_FOLDER, "model", "gpt2", "vocab.bpe"),
+            "url": urljoin(GPT2_BASE_URL, "vocab.bpe"),
+        },
+        {
+            "file": os.path.join(FINETUNE_BASE_FOLDER, "model", "gpt2", "encoder.json"),
+            "url": urljoin(GPT2_BASE_URL, "encoder.json"),
+        }
+    ]
