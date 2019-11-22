0cee6afa65cfc146419f97f2e2a3fa40d208166a
Madison May <madison@indico.io>
2019-11-22 09:18:18 -0500

    WIP

--- finetune/nn/auxiliary.py
+++ finetune/nn/auxiliary.py
@@ -61,19 +61,28 @@ def add_context_embed(featurizer_state):
 def pairwise_embed_context(context, featurizer_state, config, train):
     with tf.variable_scope("context_attn_embedding"):
         context_dim = shape_list(context)[-1]
-        diff = tf.expand_dims(context, 1) - tf.expand_dims(context, 2)
+        seq_mask = tf.sequence_mask(featurizer_state['lengths'], maxlen=config.max_length)
+        sq_mask = tf.expand_dims(seq_mask, 1) & tf.expand_dims(seq_mask, 2)
+        heads_sq_mask = tf.stack([sq_mask] * 16, axis=1) 
+        tf.summary.histogram('context 0', context[:,:,0][seq_mask])
+        diff = tf.abs(tf.expand_dims(context, 1) - tf.expand_dims(context, 2))
+        tf.summary.histogram('specific doc diff', diff[0,:,:][sq_mask[0,:,:]])
+
         g = tf.get_variable(
             name='g',
             shape=[1, config.n_heads, 1, 1, context_dim],
-            initializer=tf.random_normal_initializer(mean=1.0, stddev=config.context_embed_stddev)
+            initializer=tf.random_normal_initializer(mean=1.0, stddev=0.5)
         )
         b = tf.get_variable(
             name='b',
             shape=[1, config.n_heads, 1, 1, context_dim],
-            initializer=tf.zeros_initializer()
+            initializer=tf.constant_initializer(0.2)
         )
-        proximity = tf.nn.sigmoid(-diff)
-        proximity = tf.expand_dims(proximity, axis=1)
-        offset = proximity * g + b
-        total_offset = tf.reduce_sum(offset, axis=-1, keep_dims=False) 
+        tf.summary.histogram('g', g)
+        tf.summary.histogram('b', b)
+        tf.summary.histogram('diff', diff[sq_mask])
+        proximity = tf.nn.relu(tf.expand_dims(-diff, axis=1) * g + b)
+        tf.summary.histogram('proximity', proximity[heads_sq_mask])
+        total_offset = tf.reduce_sum(proximity, axis=-1, keep_dims=False) 
+        tf.summary.histogram('mean_offset', total_offset[heads_sq_mask])
     featurizer_state['context'] = total_offset
