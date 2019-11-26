3525d81ad4c154cae5f4f9e8b947c40baab53637
Madison May <madison@indico.io>
2019-11-26 12:49:21 -0500

    WIP: target model lr modification

--- finetune/model.py
+++ finetune/model.py
@@ -280,6 +280,7 @@ def get_model_fn(
                     epsilon=params.epsilon,
                     weight_decay=params.l2_reg * lr,
                 )
+                opt.target_model_lr_mult = params.target_model_lr_mult
                 decay_var_list = [
                     v
                     for v in tf.global_variables()
