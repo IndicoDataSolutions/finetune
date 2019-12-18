aeca3b80fd6facad2f55964835dd7c06d6fe2e1b
Astha Patni <astha.patni@indico.io>
2019-12-18 18:33:19 -0500

    FIX: imbalanced learn

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ from setuptools import setup, find_packages
 
 
 REQUIREMENTS = [
-    "pandas>=0.23.1",
-    "tqdm>=4.0.0",
+    "pandas==0.25.2",
+    "tqdm==4.36.1",
     "numpy>=1.13.0",
-    "scipy>=1.1.0",
+    "scipy==1.3.1",
     "scikit-learn>=0.18.0",
     "ftfy>=4.4.0",
-    "spacy>=2.0.0",
+    "spacy==2.2.0",
     "h5py>=2.8.0",
     "joblib>=0.12.0",
     "bs4>=0.0.1",
     "imblearn>=0.0",
+    "imbalanced-learn==0.5.0",
     "nltk>=3.2.4",
-    "regex>=2019.03.12",
+    "regex==2019.8.20",
     "lxml>=4.3.3"
 ]
 
