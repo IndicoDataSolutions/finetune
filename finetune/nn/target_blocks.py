4a6615876873a2681d0085c216ec656308263bfb
Madison May <madison@indico.io>
2020-01-02 09:06:36 -0500

    target reweighting instead of example reweighting

--- finetune/nn/target_blocks.py
+++ finetune/nn/target_blocks.py
@@ -152,9 +152,9 @@ def _apply_multilabel_class_weight(losses, targets, class_weights=None):
         # loss multiplier applied based on true class
         weights = (
             # contribution of positive class
-            tf.reduce_sum(class_weights * tf.to_float(targets), axis=1) + 
+            class_weights * tf.to_float(targets) + 
             # contribution of negative class
-            tf.reduce_sum(tf.ones_like(class_weights) * (1 - tf.to_float(targets)), axis=1)
+            tf.ones_like(class_weights) * (1 - tf.to_float(targets))
         )
         weights *= tf.to_float(tf.reduce_prod(tf.shape(weights))) / tf.reduce_sum(
             weights
@@ -262,9 +262,8 @@ def multi_classifier(
             clf_losses = tf.nn.sigmoid_cross_entropy_with_logits(
                 logits=clf_logits, labels=tf.stop_gradient(targets)
             )
-            per_example_losses = tf.reduce_sum(clf_losses, axis=-1)
             clf_losses = _apply_multilabel_class_weight(
-                per_example_losses, targets, kwargs.get("class_weights")
+                clf_losses, targets, kwargs.get("class_weights")
             )
         return {"logits": clf_logits, "losses": clf_losses}
 
