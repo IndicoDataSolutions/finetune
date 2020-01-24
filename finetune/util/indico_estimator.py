5b45231cdc9492c30ef34f7f6857d3ca20b3217d
benleetownsend <benlt@hotmail.co.uk>
2020-01-24 16:35:53 +0000

    WIP: faster re-feeding

--- finetune/util/indico_estimator.py
+++ finetune/util/indico_estimator.py
@@ -24,8 +24,10 @@ class IndicoEstimator(tf.estimator.Estimator):
         self.features_real = None
         self.placeholder_feats = None
         self.predictions = None
+        self.ds_init = None
         self.mon_sess = None
         self._cached_predict = False
+        self.available_data = []
         super().__init__(*args, **kwargs)
 
     def get_features_from_fn(self, input_fn, predict=True):
@@ -36,19 +38,22 @@ class IndicoEstimator(tf.estimator.Estimator):
                 features = features[0]
             with tf.Session() as sess:
                 sess.run(initializer)
-                output = []
                 while True:
                     try:
-                        output.append(sess.run(features))
+                        self.available_data.append(sess.run(features))
                     except tf.errors.OutOfRangeError:
                         break
-            return output, features
+            return features
+
+    def data_generator(self):
+        yield from self.available_data
+        self.available_data = []
 
     def close_predict(self):
         #tf.reset_default_graph()
         self.estimator_spec = None
         self.features_real = None
-        self.placeholder_feats = None
+        self.ds_init = None
         self.predictions = None
         self.g = None
         if self.mon_sess is not None:
@@ -64,7 +69,7 @@ class IndicoEstimator(tf.estimator.Estimator):
         # Check that model has been trained.
         self.g = self.g or tf.Graph()
         random_seed.set_random_seed(self._config.tf_random_seed)
-        features_real, features = self.get_features_from_fn(input_fn)
+        features = self.get_features_from_fn(input_fn)
         with self.g.as_default():
             if self.estimator_spec is None:
                 self._create_and_assert_global_step(self.g)
@@ -74,15 +79,21 @@ class IndicoEstimator(tf.estimator.Estimator):
                 if not checkpoint_path:
                     logging.info('Could not find trained model in model_dir: {}, running '
                                  'initialization to predict.'.format(self._model_dir))
-
-                self.placeholder_feats = tf.contrib.framework.nest.map_structure(placeholder_like, features)
-                self.estimator_spec = self._call_model_fn(
-                    self.placeholder_feats, None, ModeKeys.PREDICT, self.config)
+                with tf.device('/cpu:0'):
+                    iterator = tf.data.make_initializable_iterator(
+                        tf.data.Dataset.from_generator(
+                            self.data_generator,
+                            tf.contrib.framework.nest.map_structure(lambda f: f.dtype, features),
+                            tf.contrib.framework.nest.map_structure(lambda f: f.shape, features),
+                        )
+                    )
+                self.ds_init = iterator.initializer
+                model_fn_input = iterator.get_next()
+                self.estimator_spec = self._call_model_fn(model_fn_input, None, ModeKeys.PREDICT, self.config)
                 # Call to warm_start has to be after model_fn is called.
                 self._maybe_warm_start(checkpoint_path)
 
-                self.predictions = self._extract_keys(
-                    self.estimator_spec.predictions, predict_keys)
+                self.predictions = self._extract_keys(self.estimator_spec.predictions, predict_keys)
                 all_hooks = hooks or []
                 all_hooks.extend(list(self.estimator_spec.prediction_hooks or []))
 
@@ -94,20 +105,23 @@ class IndicoEstimator(tf.estimator.Estimator):
                         config=self._session_config),
                     hooks=all_hooks)
 
-            for feats in features_real:
-                feed_dict = {self.placeholder_feats[k]: v for k, v in feats.items()}
-                preds_evaluated = self.mon_sess.run(self.predictions, feed_dict=feed_dict)
-                if not yield_single_examples:
-                    yield preds_evaluated
-                elif not isinstance(self.predictions, dict):
-                    for pred in preds_evaluated:
-                        yield pred
-                else:
-                    for i in range(self._extract_batch_length(preds_evaluated)):
-                        yield {
-                            key: value[i]
-                            for key, value in preds_evaluated.items()
-                        }
+            self.mon_sess.run(self.ds_init)
+            while True:
+                try:
+                    preds_evaluated = self.mon_sess.run(self.predictions)
+                    if not yield_single_examples:
+                        yield preds_evaluated
+                    elif not isinstance(self.predictions, dict):
+                        for pred in preds_evaluated:
+                            yield pred
+                    else:
+                        for i in range(self._extract_batch_length(preds_evaluated)):
+                            yield {
+                                key: value[i]
+                                for key, value in preds_evaluated.items()
+                            }
+                except tf.errors.OutOfRangeError:
+                    self.available_data = []
 
 class Scheduler:
     def __init__(self, models, max_models_per_gpu, num_gpus):
