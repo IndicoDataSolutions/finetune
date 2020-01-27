52d79303da7a58b54c7cc51afcfa790cc1de5117
benleetownsend <benlt@hotmail.co.uk>
2020-01-27 17:09:52 +0000

    ADD: benchmarks

---
+++ speed_benchmarks/run.py
@@ -0,0 +1,51 @@
+import time
+
+from tabulate import tabulate
+from finetune import Classifier, SequenceLabeler
+from finetune.base_models import RoBERTa
+from synthetic_data import multi_label_sequence_data, sequence_data, classification_data
+
+def benchmark(model_cls, config, x, y, runs):
+    train_time = 0
+    inference_time = 0	
+    for	_ in range(runs):
+        model = model_cls(**config)
+        train_start_time = time.time()
+        model.fit(x, y)
+        train_time += time.time() - train_start_time	
+
+        infer_start_time = time.time()
+        model.predict(x)
+        inference_time += time.time() - infer_start_time
+    return train_time / runs, inference_time / runs
+
+def benchmark_classification(config, runs=5):
+    x, y = classification_data()
+    return benchmark(Classifier, config, x, y, runs=runs)
+
+def benchmark_sequence(config, runs=5):
+    x, y = sequence_data()
+    return benchmark(SequenceLabeler, config, x, y, runs=runs)
+
+def benchmark_multi_sequence(config, runs=5):
+    x, y = multi_label_sequence_data()
+    config["mutlilabel_sequence"] = True
+    return benchmark(SequenceLabeler, config, x, y, runs=runs)
+
+if __name__ == "__main__":
+    runs = 1
+    base_config = {"base_model": RoBERTa}
+    output = [["Model", "Optimized For", "Train Time", "Predict Time"]]
+    for optimize_for in ["speed", "accuracy"]:
+        config = dict(base_config)
+        config["optimize_for"] = optimize_for
+        clf_train, clf_infer = benchmark_classification(config, runs=runs)
+        output.append(["Classification", optimize_for, clf_train, clf_infer])
+
+        seq_train, seq_infer = benchmark_sequence(config, runs=runs)
+        output.append(["Sequence", optimize_for, seq_train, seq_infer])
+
+        mutli_seq_train, mutli_seq_infer = benchmark_multi_sequence(config, runs=runs)
+        output.append(["Multi Sequence", optimize_for, multi_seq_train, multi_seq_infer])
+    print(tabulate(output))
+    
