0707fa4b6ddd9ae18fd87c056e1e396b69d86d39
benleetownsend <benlt@hotmail.co.uk>
2020-01-28 14:39:42 +0000

    ADD: scheduler and test

---
+++ finetune/scheduler.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from finetune import Classifier
+
+class Scheduler:
+    def __init__(self, max_models=None):
+        self.loaded_models = list() # model queue
+        self.max_models = max_models
+        self.session = None
+        self.gpu_memory_limit = None
+        self.in_use_op = tf.contrib.memory_stats.BytesInUse()
+        self.peak_mem_op = tf.contrib.memory_stats.BytesInUse()
+        self.model_cache = dict()
+        
+    def _memory_for_one_more(self):
+        if self.session is None:
+            return True # First prediction run?
+        in_use, peak = self.session.run((self.in_use_op, self.peak_mem_op))
+        avg_model_size = in_use / len(self.loaded_models)
+        return (2 * avg_model_size) + peak < self.gpu_memory_limit
+
+    def _rotate_in_model(self, model):
+        if model not in self.loaded_models:
+            if (
+                    self.max_models is not None and len(self.loaded_models) + 1 > self.max_models_per_gpu or
+                    not self._memory_for_one_more()
+            ):
+                name = self.loaded_models.pop(0)
+                self.model_cache[name].close()
+                del self.model_cache[name]
+            out_model =	Classifier.load(model) # doesn't matter its classifier                                                                        
+            self.model_cache[model] = out_model
+
+        else:
+            out_model = self.model_cache[model]
+            self.loaded_models.remove(model) # put it back at the end of the queue
+            
+        self.loaded_models.append(model)
+        out_model._cached_predict = True
+        
+        return out_model
+
+    def predict(self, model_file, x):
+        model = self._rotate_in_model(model_file)
+        predictions = model.predict(x)
+        if self.session is None:
+            self.session = tf.Session() # delay this so that any options get applied from finetune.
+            self.gpu_memory_limit = self.session.run(tf.contrib.memory_stats.BytesLimit())
+        return predictions
