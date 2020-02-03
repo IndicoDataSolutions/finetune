077b2bd213239573b7e0a710f8624d4be75e8edc
Concomitant <daniel.p.shank@gmail.com>
2020-02-03 11:21:35 -0500

    print statements etc for developing generate_text

--- finetune/model.py
+++ finetune/model.py
@@ -17,6 +17,7 @@ from finetune.errors import FinetuneError
 from finetune.base_models import GPTModel, GPTModelSmall
 from finetune.optimizers.adafactor import AdafactorWOptimizer, AdafactorOptimizer
 from finetune.nn.auxiliary import embed_context
+from finetune.nn.target_blocks import gather_indexes
 
 LOGGER = logging.getLogger("finetune")
 
@@ -32,6 +33,8 @@ class PredictMode:
     NORMAL = "NORM"
     PROBAS = "PROBA"
     GENERATE_TEXT = "GEN_TEXT"
+    MLM_IDS = "mlm_ids"
+    MLM_POSITIONS = "mlm_positions"
     LM_PERPLEXITY = "PERPLEXITY"
     ATTENTION = "ATTENTION"
     SEQUENCE = "SEQUENCE"
@@ -71,8 +74,8 @@ def language_model_op(X, M, params, featurizer_state, mode, encoder):
     return lm_predict_op, language_model_state
 
 
-def masked_language_model_op(X, M, mlm_weights, mlm_ids, mlm_positions, params, featurizer_state, mode):
-    return masked_language_model(
+def masked_language_model_op(X, M, mlm_weights, mlm_ids, mlm_positions, params, featurizer_state, mode, encoder):
+    language_model_state = masked_language_model(
         X=X,
         M=M,
         mlm_weights=mlm_weights,
@@ -83,8 +86,32 @@ def masked_language_model_op(X, M, mlm_weights, mlm_ids, mlm_positions, params, 
         hidden=featurizer_state["sequence_features"],
         train=(mode == tf.estimator.ModeKeys.TRAIN)
     )
-    return language_model_state
+    lm_logits = language_model_state['logits']
 
+    lm_logit_mask = np.zeros(
+        [1, lm_logits.get_shape().as_list()[-1]], dtype=np.float32
+    )
+    lm_logit_mask[:, encoder.vocab_size :] = -np.inf
+
+    if "use_extra_toks" in params and not params.use_extra_toks:
+        lm_logit_mask[:, encoder.start_token] = -np.inf
+        lm_logit_mask[:, encoder.delimiter_token] = -np.inf
+        lm_logit_mask[:, encoder.end_token] = -np.inf
+
+        lm_logits += lm_logit_mask
+
+
+
+    relevant_logits = tf.boolean_mask(lm_logits, tf.reshape(mlm_weights, shape=(-1,)))
+    #relevant_logits = tf.reshape(lm_logits, shape=(-1,))
+    #relevant_ids = tf.boolean_mask(mlm_ids, tf.reshape(mlm_weights, shape=(-1,)))
+    #relevant_positions = tf.boolean_mask(mlm_positions, tf.reshape(mlm_weights, shape=(-1,)))
+    relevant_ids = tf.boolean_mask(mlm_ids, mlm_weights)
+    relevant_positions = tf.boolean_mask(mlm_positions, mlm_weights)
+
+    top_token_idxs = tf.argsort(relevant_logits, direction='ASCENDING', axis=-1)
+    return (top_token_idxs, relevant_ids, relevant_positions), language_model_state
+    #return best_token_idxs, language_model_state
 
 def get_model_fn(
     target_model_fn,
@@ -120,7 +147,7 @@ def get_model_fn(
                 label_encoder=label_encoder,
                 **kwargs
             )
-            
+
         return target_model_state
 
     def _model_fn(features, labels, mode, params):
@@ -150,7 +177,7 @@ def get_model_fn(
             if context is not None:
                 featurizer_state = embed_context(context, featurizer_state, params, train)
             predictions = {
-                PredictMode.FEATURIZE: featurizer_state["features"], 
+                PredictMode.FEATURIZE: featurizer_state["features"],
                 PredictMode.SEQUENCE: featurizer_state["sequence_features"]
             }
 
@@ -192,31 +219,29 @@ def get_model_fn(
                     else:
                         predictions[PredictMode.NORMAL] = pred_op
                         predictions[PredictMode.PROBAS] = pred_proba_op
-
                     if build_explain:
                         predictions[PredictMode.EXPLAIN] = target_model_state[
                             "explanation"
                         ]
-
             if lm_type is not None:
                 if lm_type.lower() == 'lm':
                     lm_predict_op, language_model_state = language_model_op(
                         X=X, M=M, params=params, featurizer_state=featurizer_state, mode=mode, encoder=encoder
                     )
+
                 elif lm_type.lower() == 'mlm':
-                    language_model_state = masked_language_model_op(
-                        X=X, 
-                        M=M, 
+                    (lm_predict_op, mlm_ids, mlm_positions), language_model_state = masked_language_model_op(
+                        X=X,
+                        M=M,
                         mlm_weights=features['mlm_weights'],
                         mlm_ids=features['mlm_ids'],
                         mlm_positions=features['mlm_positions'],
-                        params=params, 
+                        params=params,
                         featurizer_state=featurizer_state,
-                        mode=mode
+                        mode=mode,
+                        encoder=encoder
                     )
-                    # No support for any form of text generation for MLM for now
-                    lm_predict_op = None
-                else: 
+                else:
                     raise FinetuneError("Unsupport `lm_type` option: {}".format(lm_type))
 
                 if (
@@ -229,9 +254,15 @@ def get_model_fn(
                 if mode == tf.estimator.ModeKeys.PREDICT:
                     if lm_predict_op is not None:
                         predictions[PredictMode.GENERATE_TEXT] = lm_predict_op
-                    predictions[PredictMode.LM_PERPLEXITY] = language_model_state[
-                        "perplexity"
-                    ]
+                        if lm_type.lower() == 'mlm':
+                            predictions[PredictMode.MLM_IDS] = mlm_ids
+                            predictions[PredictMode.MLM_POSITIONS] = mlm_positions
+                            #predictions[PredictMode.MLM_IDS] = features['mlm_ids']
+                            #predictions[PredictMode.MLM_POSITIONS] = features['mlm_positions']
+                    if lm_type.lower == 'lm':
+                        predictions[PredictMode.LM_PERPLEXITY] = language_model_state[
+                            "perplexity"
+                        ]
 
         if mode == tf.estimator.ModeKeys.TRAIN:
             total_num_steps = params.n_epochs * params.dataset_size // (params.batch_size * n_replicas)
@@ -430,4 +461,3 @@ def get_separate_model_fns(
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
     return _target_model_fn
-
