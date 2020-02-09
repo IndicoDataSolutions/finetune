abf5f6126b35f0e0bff65fb65a6ffbaf0d7a2c77
lily <lilyhzhang1029@gmail.com>
2020-02-08 22:10:37 -0500

    ADD: use end context when context doesn't line up

--- finetune/encoding/input_encoder.py
+++ finetune/encoding/input_encoder.py
@@ -223,7 +223,9 @@ def tokenize_context(context, encoded_output, config):
         else:
             if char_loc > context_by_char_loc[current_char_loc][0]:
                 current_char_loc += 1
-                if current_char_loc >= len(context_by_char_loc): 
+                if current_char_loc >= len(context_by_char_loc):
+                    # TODO: this is a workaround that has no guarantees of being correct
+                    current_char_loc = context_by_char_loc - 1
                     raise ValueError("Context cannot be fully matched as it appears to not cover the end of the sequence")
 
             tokenized_context.append(context_by_char_loc[current_char_loc][1])
