fc96ac9b8706f1d8ce41684c74a5d5330cd30ff2
Lily Zhang <lily.zhang@indico.io>
2020-02-21 17:49:13 -0500

    CLEAN: remove commented code

--- finetune/saver.py
+++ finetune/saver.py
@@ -127,13 +127,10 @@ class InitializeHook(SessionRunHook):
         elif self.model_portion == "entire_model":
             self.init_fn(None, session, self.model_portion)
         # import ipdb; ipdb.set_trace()
-        mw = self.saver.fallback_
-        gw = [w.name for w in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)]
-        print(len(set(mw) - set(gw)))
-        print(set(mw) - set(gw))
-        print(len(set(gw) - set(mw)))
-        # import sys
-        # sys.exit()
+        # mw = self.saver.fallback_
+        # gw = [w.name for w in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)]
+        # print(len(set(mw) - set(gw)))
+        # print(len(set(gw) - set(mw)))
 
     def before_run(self, run_context):
         if "featurizer" in self.model_portion and (
