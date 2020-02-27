FIX: whole word masking doesn't fall off page

finetune/target_models/masked_language_model.py
@@ -13,10 +13,7 @@ from tensorflow.data import Dataset
 
 
 def get_mask(seq_len, config):
-    if config.mask_spans > 1:
-        mask_proba = config.mask_proba / config.mask_spans
-    else:
-        mask_proba = config.mask_proba
+    mask_proba = config.mask_proba / config.mask_spans
 
     a = np.random.rand(seq_len)
     if config.table_mask_bias:
@@ -38,13 +35,13 @@ def get_mask(seq_len, config):
 
 def mask_out_whole_words(mlm_mask, out, seq_len):
     # extend end of mask spans to word boundaries
-    for i in range(seq_len):
+    for i in range(seq_len - 1):
         curr_end = out.token_ends[i]
         next_start = out.token_starts[i + 1]
         if mlm_mask[i] and not mlm_mask[i + 1] and curr_end == next_start:
             mlm_mask[i + 1] = True
     # extend start of mask spans to word boundaries
-    for j in reversed(range(seq_len)):
+    for j in reversed(range(1, seq_len)):
         curr_start = out.token_starts[i]
         prev_end = out.token_starts[i - 1]
         if mlm_mask[j] and not mlm_mask[j - 1] and curr_start == prev_end:

tests/test_masked_language_model.py
@@ -15,7 +15,7 @@ from finetune import MaskedLanguageModel, Classifier
 from finetune.errors import FinetuneError
 from finetune.base_models import GPT2, BERT, RoBERTa
 from finetune.config import Settings
-from finetune.target_models.masked_language_model import _get_mask
+from finetune.target_models.masked_language_model import get_mask
 
 
 class TestMaskedLanguageModel(unittest.TestCase):
@@ -117,7 +117,7 @@ class TestMaskedLanguageModel(unittest.TestCase):
         ]
         np.random.seed(1)
         for config in configs:
-            mlm_mask = _get_mask(500, config)
+            mlm_mask = get_mask(500, config)
             self.assertEqual(len(mlm_mask), 500)
             if config.table_mask_bias:
                 self.assertGreater(np.mean(mlm_mask[:20]), np.mean(mlm_mask[-20:]))