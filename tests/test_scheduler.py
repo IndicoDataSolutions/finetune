a1c7495b295169247cfb2cc6bebdf0935f801595
Madison May <madison@indico.io>
2020-03-18 14:53:26 -0400

    ADD: featurize support under cached_predict

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -55,3 +55,6 @@ class TestScheduler(unittest.TestCase):
         time2_end = time.time()
         self.assertLess(time2_end - time2_start, time_mid - time_pre - 1) # Assert that it is still quicker.
         shed.predict_proba(m1, ["A"])
+        shed.featurize(m1, ["A"])
+        shed.featurize(m1, ["A"])
+        shed.featurize_sequence(m1, ["A"])
