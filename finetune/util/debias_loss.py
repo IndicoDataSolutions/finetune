ff6bd486e2fb408ae26f3db245f908c7ea9bf130
lily <lilyhzhang1029@gmail.com>
2020-03-19 00:40:03 -0400

    ADD: cross_entropy contrib function to pass in weights

--- finetune/util/debias_loss.py
+++ finetune/util/debias_loss.py
@@ -20,13 +20,13 @@ class ClfDebiasLossFunction(Configured):
 
 class Plain(ClfDebiasLossFunction):
   def compute_clf_loss(self, hidden, logits, bias, labels, weights=None):
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, weights=weights)
+    loss = tf.compat.v1.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels, weights=weights)
     return tf.reduce_mean(loss)
 
 
 class Reweight(ClfDebiasLossFunction):
   def compute_clf_loss(self, hidden, logits, bias, labels, weights=None):
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, weights=weights)
+    loss = tf.compat.v1.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels, weights=weights)
     label_one_hot = tf.one_hot(labels, ops.get_shape_tuple(logits, 1))
     weights = 1 - tf.reduce_sum(tf.exp(bias) * label_one_hot, 1)
     return tf.reduce_sum(weights*loss) / tf.reduce_sum(weights)
@@ -35,7 +35,7 @@ class Reweight(ClfDebiasLossFunction):
 class BiasProduct(ClfDebiasLossFunction):
   def compute_clf_loss(self, hidden, logits, bias, labels, weights=None):
     logits = tf.nn.log_softmax(logits)
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits+bias, labels=labels, weights=weights)
+    loss = tf.compat.v1.losses.sparse_softmax_cross_entropy(logits=logits+bias, labels=labels, weights=weights)
     return tf.reduce_mean(loss)
 
 
@@ -51,7 +51,7 @@ class LearnedMixin(ClfDebiasLossFunction):
     factor = tf.nn.softplus(factor)
     bias *= tf.expand_dims(factor, 1)
 
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    loss = tf.compat.v1.losses.sparse_softmax_cross_entropy(
       logits=logits+bias, labels=labels, weights=weights)
     loss = tf.reduce_mean(loss)
 
