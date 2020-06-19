1deb79e60ad894fc43303a70ba21dac4a4495036
Eamon Ito-Fisher <eamon@itofisher.com>
2020-06-19 00:00:49 -0700

    ADD: k best veterbi algorithm. UNTESTED - MIGHT NOT RUN

--- finetune/nn/crf.py
+++ finetune/nn/crf.py
@@ -36,6 +36,80 @@ def viterbi_decode(score, transition_params):
 
     return viterbi, np_softmax(trellis, axis=-1)
 
+def top_k_0_axis(matrix, k):
+    matrix = tf.transpose(matrix)
+    scores, indicies = tf.math.top_k(matrix, k=k)
+    scores = tf.transpose(scores)
+    indicies = tf.transpose(indicies)
+    return scores, indicies
+
+def k_viterbi_decode(tag_sequence, transition_matrix, top_k=5):
+    """
+    Perform Viterbi decoding in log space over a sequence given a transition matrix
+    specifying pairwise (transition) potentials between tags and a matrix of shape
+    (sequence_length, num_tags) specifying unary potentials for possible tags per
+    timestep.
+    Parameters
+    ----------
+    tag_sequence : torch.Tensor, required.
+        A tensor of shape (sequence_length, num_tags) representing scores for
+        a set of tags over a given sequence.
+    transition_matrix : torch.Tensor, required.
+        A tensor of shape (num_tags, num_tags) representing the binary potentials
+        for transitioning between a given pair of tags.
+    top_k : int, required.
+        Integer defining the top number of paths to decode.
+    Returns
+    -------
+    viterbi_path : List[int]
+        The tag indices of the maximum likelihood tag sequence.
+    viterbi_score : float
+        The score of the viterbi path.
+    """
+    sequence_length, num_tags = list(tag_sequence.shape())
+
+    path_scores = []
+    path_indices = []
+    # At the beginning, the maximum number of permutations is 1; therefore, we unsqueeze(0)
+    # to allow for 1 permutation.
+    path_scores.append(tf.expand_dims(tag_sequence[0, :], 0))
+    # assert path_scores[0].size() == (n_permutations, num_tags)
+
+    # Evaluate the scores for all possible paths.
+    for timestep in range(1, sequence_length):
+        # Add pairwise potentials to current scores.
+        # assert path_scores[timestep - 1].size() == (n_permutations, num_tags)
+        summed_potentials = tf.expand_dims(path_scores[timestep - 1], 2) \
+                + transition_matrix
+        summed_potentials = tf.reshape(summed_potentials, (-1, num_tags))
+
+        # Best pairwise potential path score from the previous timestep. 
+        max_k = min(summed_potentials.shape()[0], top_k)
+        scores, paths = top_k_0_axis(summed_potentials, max_k)
+        # assert scores.size() == (n_permutations, num_tags)
+        # assert paths.size() == (n_permutations, num_tags)
+
+        scores = tag_sequence[timestep, :] + scores
+        # assert scores.size() == (n_permutations, num_tags)
+        path_scores.append(scores)
+        path_indices.append(paths.squeeze())
+
+    # Construct the most likely sequence backwards.
+    path_scores = tf.reshape(path_scores[-1], (-1,))
+    max_k = min(path_scores.shape()[0], top_k)
+
+    viterbi_scores, best_paths = top_k_0_axis(path_scores, max_k)
+    viterbi_paths = []
+    for i in range(max_k):
+        viterbi_path = [best_paths[i]]
+        for backward_timestep in reversed(path_indices):
+            viterbi_path.append(int(tf.reshape(backward_timestep, (-1,))[viterbi_path[-1]]))
+        # Reverse the backward path.
+        viterbi_path.reverse()
+        # Viterbi paths uses (num_tags * n_permutations) nodes; therefore, we need to modulo.
+        viterbi_path = [j % num_tags for j in viterbi_path]
+        viterbi_paths.append(viterbi_path)
+    return viterbi_paths, viterbi_scores
 
 def sequence_decode(logits, transition_matrix, sequence_length, use_gpu_op, use_crf):
     """ A simple py_func wrapper around the Viterbi decode allowing it to be included in the tensorflow graph. """
