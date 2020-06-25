16a9f5d3a5c9e4db8b6e65c08479c77abe8c32d5
benleetownsend <benlt@hotmail.co.uk>
2020-06-25 11:06:01 +0100

    FIX: Refactor the global_step stuff in grad accumulation

--- finetune/optimizers/gradient_accumulation.py
+++ finetune/optimizers/gradient_accumulation.py
@@ -21,6 +21,8 @@ def get_grad_accumulation_optimizer(optimizer_class, accum_steps):
             super().__init__(*args, **kwargs)
 
         def apply_gradients(self, grads_and_vars, global_step=None, name=None, *args, **kwargs):
+            # If global step is set we should increment it as per tf1 optimizers
+            global_step_set = global_step is not None
             add_gradients_ops = []
             accumulation_vars = []
             grads_and_accumulated_vars = []
@@ -44,8 +46,8 @@ def get_grad_accumulation_optimizer(optimizer_class, accum_steps):
 
                 grads_and_accumulated_vars.append((accum_grad, v))
 
-            global_step_var = global_step if global_step is not None else tf.compat.v1.train.get_or_create_global_step()
-            if global_step is not None:
+            global_step = global_step if global_step_set else tf.compat.v1.train.get_or_create_global_step()
+            if global_step_set:
                 kwargs["global_step"] = global_step
             with tf.control_dependencies(add_gradients_ops):
                 def apply_grads():
@@ -58,9 +60,9 @@ def get_grad_accumulation_optimizer(optimizer_class, accum_steps):
                     return apply_grads_op
 
                 return tf.cond(
-                    pred=tf.equal(global_step_var % accum_steps, accum_steps - 1),
+                    pred=tf.equal(global_step % accum_steps, accum_steps - 1),
                     true_fn=apply_grads,
-                    false_fn=lambda: tf.no_op() if global_step is None else tf.group(global_step.assign_add(1))
+                    false_fn=lambda: tf.no_op() if global_step_set else tf.group(global_step.assign_add(1))
                 )
 
     return GradAccumulationOptimizer
