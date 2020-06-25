5da0fe5b2cd30770b8d5299c93beb4ef3b077107
benleetownsend <benlt@hotmail.co.uk>
2020-06-25 10:58:03 +0100

    ADD: fix final multigpu bug

--- finetune/optimizers/weight_decay.py
+++ finetune/optimizers/weight_decay.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+from tensorflow.python.ops import resource_variable_ops
+
 
 class AdamW(tf.compat.v1.train.AdamOptimizer):
-   
     def __init__(self, learning_rate, *, weight_decay, decay_var_list, **kwargs):
         """Extension class that adds weight decay to an optimizer.
         Args:
@@ -16,16 +17,12 @@ class AdamW(tf.compat.v1.train.AdamOptimizer):
 
     def _decay_weights_op(self, var):
         if self.decay_var_list is None or var in self.decay_var_list:
-            return var.assign_sub(
-                self.wd * var, self._use_locking
-            )
+            return var.assign_sub(self.wd * var, self._use_locking)
         return tf.no_op()
 
     def _decay_weights_sparse_op(self, var, indices):
         if self.decay_var_list is None or var in self.decay_var_list:
-            update = -self.wd * tf.gather(
-                var, indices
-            )
+            update = -self.wd * tf.gather(var, indices)
             return self._resource_scatter_add(var, indices, update)
         return tf.no_op()
 
@@ -37,3 +34,9 @@ class AdamW(tf.compat.v1.train.AdamOptimizer):
         decay_op = self._decay_weights_sparse_op(var, indices)
         with tf.control_dependencies([decay_op]):
             return super()._resource_apply_sparse(grad, var, indices)
+
+    def _resource_scatter_add(self, x, i, v):
+        with tf.control_dependencies(
+            [resource_variable_ops.resource_scatter_add(x.handle, i, v)]
+        ):
+            return tf.identity(x)
