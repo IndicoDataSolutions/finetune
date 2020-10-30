08b9d3d126d1384c571486ca3d223eac6df62164
Madison May <madison@indico.io>
2020-10-29 20:49:27 -0400

    FIX: label rounding behavior

--- finetune/encoding/target_encoders.py
+++ finetune/encoding/target_encoders.py
@@ -192,7 +192,7 @@ class SequenceLabelingEncoder(BaseEncoder):
         self.lookup = None
 
     def fit(self, labels):
-        self.classes_ = list(set(lab_i["label"] for lab in labels for lab_i in lab) | {self.pad_token})
+        self.classes_ = sorted(list(set(lab_i["label"] for lab in labels for lab_i in lab) | {self.pad_token}))
         self.lookup = {c: i for i, c in enumerate(self.classes_)}
 
     def pre_process_label(self, out, labels):
@@ -222,7 +222,8 @@ class SequenceLabelingEncoder(BaseEncoder):
         labels_out = [pad_idx for _ in out.tokens]
         for label in labels:
             for i, (start, end, text) in enumerate(zip(out.token_starts, out.token_ends, out.tokens)):
-                if start > label["end"]:
+                # Label extends less than halfway through token
+                if label["end"] < (start + end + 1) // 2:
                     break
                 overlap, agree = self.overlaps(label, start, end, text)
                 if overlap:
---
+++ tests/test_sequence_labeling_encoder.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+from finetune.encoding.input_encoder import EncodedOutput
+from finetune.encoding.target_encoders import SequenceLabelingEncoder
+
+def test_sequence_label_encoder():
+    encoder = SequenceLabelingEncoder(pad_token="<PAD>")
+    labels = [{'start': 13, 'end': 16, 'label': 'z', 'text': '(5%)'}]
+    encoder.fit([labels])
+    out = EncodedOutput(
+        token_ids=np.array([   0, 9583,  139,   40,  249, 8875,    2]), 
+        tokens=np.array(['0', 'five', ' percent', ' (', '5', '%)', '2'], dtype='<U21'), 
+        token_ends=np.array([-1,  4, 12, 14, 15, 17, -1]), 
+        token_starts=np.array([-1,  0,  5, 13, 14, 15, -1]), 
+        useful_start=0, 
+        useful_end=512
+    )
+    label_arr = encoder.transform(out, labels)
+    assert label_arr == [0, 0, 0, 1, 1, 1, 0]
+    
+
+def test_sequence_label_encoder_does_not_exceed_half():
+    encoder = SequenceLabelingEncoder(pad_token="<PAD>")
+    labels = [{'start': 0, 'end': 2, 'label': 'z', 'text': 'to'}]
+    encoder.fit([labels])
+    out = EncodedOutput(
+        token_ids=np.array([    0, 46661,     2]), 
+        tokens=np.array(['0', 'token', '2'], dtype='<U21'), 
+        token_ends=np.array([-1,  5, -1]), 
+        token_starts=np.array([-1,  0, -1]), 
+        useful_start=0, 
+        useful_end=512
+    )
+    label_arr = encoder.transform(out, labels)
+    assert label_arr == [0, 0, 0]
+    
+
+def test_sequence_label_encoder_exceeds_half():
+    encoder = SequenceLabelingEncoder(pad_token="<PAD>")
+    labels = [{'start': 0, 'end': 3, 'label': 'z', 'text': 'tok'}]
+    encoder.fit([labels])
+    out = EncodedOutput(
+        token_ids=np.array([    0, 46661,     2]), 
+        tokens=np.array(['0', 'token', '2'], dtype='<U21'), 
+        token_ends=np.array([-1,  5, -1]), 
+        token_starts=np.array([-1,  0, -1]), 
+        useful_start=0, 
+        useful_end=512
+    )
+    label_arr = encoder.transform(out, labels)
+    assert label_arr == [0, 1, 0]
+    
+
+def test_sequence_label_encoder_half():
+    encoder = SequenceLabelingEncoder(pad_token="<PAD>")
+    labels = [{'start': 0, 'end': 3, 'label': 'z', 'text': 'str'}]
+    encoder.fit([labels])
+    out = EncodedOutput(
+        token_ids=np.array([   0, 8660,    2]), 
+        tokens=np.array(['0', 'stream', '2'], dtype='<U21'),
+        token_ends=np.array([-1,  6, -1]),
+        token_starts=np.array([-1,  0, -1]), 
+        useful_start=0,
+        useful_end=512
+    )
+    label_arr = encoder.transform(out, labels)
+    assert label_arr == [0, 1, 0]
