f059838797b876c35e85815d3e2ce3b964fa1006
benleetownsend <benlt@hotmail.co.uk>
2021-01-04 16:39:32 +0000

    FIX: slightly cleaner compute capaibility reading

--- finetune/util/gpu_info.py
+++ finetune/util/gpu_info.py
@@ -2,34 +2,34 @@ import re
 import logging
 
 from tensorflow.python.client import device_lib
+from tensorflow.python.framework import config
+
 
 LOGGER = logging.getLogger("finetune")
 
+def fp16_capable():
+    gpus = config.list_physical_devices('GPU')
+    if not gpus:
+        return False
+    gpu_details_list = [config.get_device_details(g) for g in gpus]
+    major_cc, _ = gpu_details_list[0]["compute_capability"]
+    if major_cc >= 7:
+        return True
+    if any(gpu_info["compute_capability"][0] >= 7 for gpu_info in gpu_details_list):
+        LOGGER.warning(
+            (
+                "A GPU with float 16 support is available but it is not GPU:0,"
+                " to take advantage of fp16 inference set this to the only available GPU."
+            )
+        )
+    return False
+
 def gpu_info(session_config=None):
-    def is_fp16_capable(device_desc):
-        match = re.search(r"compute capability: (\d+)\.(\d+)", device_desc)
-        if not match:
-            return False
-        return int(match.group(1)) >= 7
-    num_gpus = 0
-    fp16_capable = False
-    for local_device in device_lib.list_local_devices(session_config=session_config):
-        if local_device.device_type == "GPU":
-            num_gpus += 1
-            fp16 = is_fp16_capable(local_device.physical_device_desc)
-            if fp16 and not fp16_capable:
-                if num_gpus == 1:
-                    fp16_capable = True
-                else:
-                    LOGGER.warning(
-                        (
-                            "A GPU with float 16 support is available but it is not GPU:0,"
-                            " to take advantage of fp16 inference set this to the only available GPU. {}"
-                        ).format(local_device.physical_device_desc)
-                    )
+    is_fp16_capable = fp16_capable()
+    num_devices = device_lib.list_local_devices(session_config=session_config)
     return {
-        "n_gpus": num_gpus,
-        "fp16_inference": fp16_capable
+        "n_gpus": num_devices,
+        "fp16_inference": is_fp16_capable
     }
             
 if __name__ == "__main__":
