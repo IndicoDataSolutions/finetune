0f2e832796efaaa040d086e8d0f435d46dbec81e
Eamon Ito-Fisher <eamon@itofisher.com>
2021-02-05 11:38:09 -0800

    FIX: finetune_to_indico no longer merges distinct entities

--- finetune/target_models/sequence_labeling.py
+++ finetune/target_models/sequence_labeling.py
@@ -392,7 +392,6 @@ class SequenceLabeler(BaseModel):
                     proba_seq_masked[il:, label_idx] = 0.0
             doc_level_probas.append(np.max(proba_seq_masked, axis=0))
 
-
             for label, start_idx, end_idx, proba in zip(
                 label_seq, start_of_token_seq, end_of_token_seq, proba_seq
             ):
@@ -406,14 +405,13 @@ class SequenceLabeler(BaseModel):
                 last_end = end_idx
 
                 if self.config.bio_tagging:
-                    if label == self.config.pad_token:
-                        bio_prefix, label = None, label
-                    else:
+                    bio_prefix = None
+                    if label != self.config.pad_token:
                         bio_prefix, label = label[:2], label[2:]
 
                 # if there are no current subsequences
                 # or the current subsequence has the wrong label
-                # or bio tagging is on and we start a new entity
+                # or bio tagging is on and we have a B- tag
                 if (not doc_subseqs or label != doc_labels[-1] or per_token or
                     (self.config.bio_tagging and bio_prefix == "B-")):
                     assert start_idx <= end_idx, "Start: {}, End: {}".format(
@@ -455,6 +453,7 @@ class SequenceLabeler(BaseModel):
                     probs=[prob_dicts],
                     none_value=self.config.pad_token,
                     subtoken_predictions=self.config.subtoken_predictions,
+                    bio_tagging=self.config.bio_tagging,
                 )
                 if per_token:
                     doc_annotations.append(
