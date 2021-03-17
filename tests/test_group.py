fee3e5e100161bd8dab526e0f8953f32a2cd0a54
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-16 18:35:14 -0700

    ADD: Token relation model tests

--- tests/test_group.py
+++ tests/test_group.py
@@ -23,6 +23,8 @@ from finetune.target_models.grouping import (
     MultiLogitGroupSequenceLabeler,
     BROSLabeler,
     JointBROSLabeler,
+    TokenRelationLabeler,
+    JointTokenRelationLabeler,
 )
 
 class TestGroupingLabelers(unittest.TestCase):
@@ -225,3 +227,32 @@ class TestGroupingLabelers(unittest.TestCase):
         self.assertEqual(len(preds[0]), 5)
         self.assertEqual(len(preds[1]), 2)
         self.assertEqual(preds, labels)
+
+    def test_token_relation_tagging(self):
+        model = TokenRelationLabeler(n_epochs=16)
+        text = ("five percent (5%) \n " +
+                "fifty percent (50%) \n " +
+                "two percent (2%) \n " +
+                "nine percent (9%) \n " +
+                "three percent (3%) \n ")
+        labels = [
+            {'start': 0, 'end': 17, 'label': 'a', 'text': 'five percent (5%)'},
+            {'start': 20, 'end': 39, 'label': 'b', 'text': 'fifty percent (50%)'},
+            {'start': 42, 'end': 58, 'label': 'a', 'text': 'two percent (2%)'},
+            {'start': 61, 'end': 78, 'label': 'b', 'text': 'nine percent (9%)'},
+            {'start': 81, 'end': 99, 'label': 'a', 'text': 'three percent (3%)'},
+        ]
+        groups = [
+            {'tokens': [
+                {'start': 0, 'end': 39, 'text': 'five percent (5%) \n fifty percent (50%)'},
+            ], 'label': None},
+            {'tokens': [
+                {'start': 61, 'end': 99, 'text': 'nine percent (9%) \n three percent (3%)'},
+            ], 'label': None}
+        ]
+        labels = (labels, groups)
+        model.fit([text] * 30, [labels] * 30)
+        preds = model.predict([text])[0]
+        print(preds)
+        self.assertEqual(len(preds), 2)
+        self.assertEqual(preds, labels[1])
