FIX: gradient accumulation with vars on CPU

finetune/optimizers/gradient_accumulation.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
+import contextlib
 
 from finetune.errors import FinetuneError
 
 
-def get_grad_accumulation_optimizer(optimizer_class, accum_steps):
+# contextlib.nullcontext is available in python >= 3.10
+@contextlib.contextmanager
+def nullcontext():
+    yield None
+
+
+def get_grad_accumulation_optimizer(optimizer_class, accum_steps, accumulate_on_cpu=False):
     """
     Adds gradient accumulation to an Optimizer.
 
@@ -30,18 +37,20 @@ def get_grad_accumulation_optimizer(optimizer_class, accum_steps):
                 if g is None:
                     continue
                 g = tf.convert_to_tensor(value=g)
-                accum_grad = tf.compat.v1.get_variable(
-                    name=v.name[:-2] + "_acc",
-                    shape=g.shape,
-                    dtype=g.dtype,
-                    initializer=tf.compat.v1.constant_initializer(0),
-                    use_resource=True,
-                    trainable=False
-                )
-                try:
-                    add_gradients_ops.append(accum_grad.assign_add(g))
-                except ValueError:
-                    raise FinetuneError("GradAccumulationOptimizer does not currently support multiple GPUs")
+
+                with tf.device("cpu") if accumulate_on_cpu else nullcontext():
+                    accum_grad = tf.compat.v1.get_variable(
+                        name=v.name[:-2] + "_acc",
+                        shape=g.shape,
+                        dtype=g.dtype,
+                        initializer=tf.compat.v1.constant_initializer(0),
+                        use_resource=True,
+                        trainable=False
+                    )
+                    try:
+                        add_gradients_ops.append(accum_grad.assign_add(g))
+                    except ValueError:
+                        raise FinetuneError("GradAccumulationOptimizer does not currently support multiple GPUs")
                 accumulation_vars.append(accum_grad)
 
                 grads_and_accumulated_vars.append((accum_grad, v))

finetune/util/optimize_loss.py
@@ -38,6 +38,7 @@ def get_optimizer(
     vector_l2,
     accumulate_steps,
     mixed_precision,
+    acc_grads_on_cpu,
 ):
     Optimizer = OPTIMIZERS.get(optimizer_name, None)
     if Optimizer is None:
@@ -48,7 +49,7 @@ def get_optimizer(
         )
 
     if accumulate_steps > 1:
-        Optimizer = get_grad_accumulation_optimizer(Optimizer, accumulate_steps)
+        Optimizer = get_grad_accumulation_optimizer(Optimizer, accumulate_steps, accumulate_on_cpu=acc_grads_on_cpu)
 
     decay_var_list = [
         v
@@ -97,6 +98,7 @@ def optimize_loss(
     accumulate_steps,
     max_training_hours=None,
     colocate_gradients_with_ops=True,
+    acc_grads_on_cpu=True,
 ):
     global_step = tf.compat.v1.train.get_or_create_global_step()
 
@@ -140,6 +142,7 @@ def optimize_loss(
             vector_l2=vector_l2,
             accumulate_steps=accumulate_steps,
             mixed_precision=mixed_precision,
+            acc_grads_on_cpu=acc_grads_on_cpu,
         )
         variables = tf.compat.v1.trainable_variables()
         variables = [v for v in variables if is_numerical_tensor(v)]